9a6885f0152696ba66b248efa843fc0354e700f2
Wang Qiang <wangqiang102938@outlook.com>
2023-05-22 23:39:06 +0900

    complete

--- src/model/capture/image.py
+++
@@ -1,6 +0,0 @@
-from model.capture.capture_node import CaptureNode
-
-
-class ImageNode(CaptureNode):
-    def get_node_type(self):
-        return self.Type.IMAGE
--- src/model/capture/text.py
+++
@@ -1,6 +0,0 @@
-from model.capture.capture_node import CaptureNode
-
-
-class TextNode(CaptureNode):
-    def get_node_type(self):
-        return self.Type.TEXT
